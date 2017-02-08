3ba6d41989015a9c379e2d8f35da1ef2997a3523
Daniel Barbour <daniel.devnull@gmail.com>
2017-02-08 12:53:01 -0600

    update summary logic

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewChildren, QueryList, ElementRef, AfterViewInit } from '@angular/core';
 import * as moment from 'moment';
+// import { DirectRenderer } from '@angular/core/src/render/api';
+// Renderer,
 
 @Component({
   selector: 'my-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
 
   data = [
     {
@@ -28,8 +30,48 @@ export class HomeComponent {
       date: moment().format('llll'),
       step: 'Lead',
       pinned: true
-    }
-    ,{
+    },
+    {
+      customerName: 'Peter Burke',
+      appStatus: 'Pending',
+      dealId: 30508,
+      businessType: 'Small Business',
+      loanAmount: '$750,000',
+      date: moment().format('llll'),
+      step: 'Contract',
+      pinned: false
+    },
+    {
+      customerName: 'Peter Burke',
+      appStatus: 'Pending',
+      dealId: 30508,
+      businessType: 'Small Business',
+      loanAmount: '$750,000',
+      date: moment().format('llll'),
+      step: 'Contract',
+      pinned: false
+    },
+    {
+      customerName: 'Peter Burke',
+      appStatus: 'Pending',
+      dealId: 30508,
+      businessType: 'Small Business',
+      loanAmount: '$750,000',
+      date: moment().format('llll'),
+      step: 'Contract',
+      pinned: false
+    },
+    {
+      customerName: 'Peter Burke',
+      appStatus: 'Pending',
+      dealId: 30508,
+      businessType: 'Small Business',
+      loanAmount: '$750,000',
+      date: moment().format('llll'),
+      step: 'Contract',
+      pinned: false
+    },
+    {
       customerName: 'Peter Burke',
       appStatus: 'Pending',
       dealId: 30508,
@@ -43,6 +85,26 @@ export class HomeComponent {
 
   view = 'card';
 
+  @ViewChildren('cardHost') private cardHosts: QueryList<ElementRef>;
+
   constructor() {}
+
+  ngAfterViewInit(): void {
+    this.cardHosts.forEach((card: ElementRef) => {
+      let entryValue = card.nativeElement.attributes.entryrow.value;
+      // this.renderer.setElementStyle(card.nativeElement, 'background', 'yellow');
+      if (entryValue === 'true') {
+        console.log(entryValue);
+        console.log(card.nativeElement);
+        // this.renderer.createElement()
+      }
+    });
+  }
+
+  createElement(index) {
+    if (index % 3 === 0) {
+      console.log(index);
+    }
+  }
 
 }
