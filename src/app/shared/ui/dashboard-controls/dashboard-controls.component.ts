87efa7bc6975c484ae2933ab8f3e0f97a7df7341
Daniel Barbour <daniel.devnull@gmail.com>
2017-03-13 11:59:13 -0500

    update filter drawer

--- src/app/shared/ui/dashboard-controls/dashboard-controls.component.ts
+++ src/app/shared/ui/dashboard-controls/dashboard-controls.component.ts
@@ -30,6 +30,96 @@ export class DashboardControlsComponent {
   @Input() view: string;
   @Output() viewUpdated = new EventEmitter();
   filtersOpen = false;
+  dealers = [
+    {
+      name: 'Dealership 1',
+      value: '1Deal'
+    },
+    {
+      name: 'Dealership 2',
+      value: '2Deal'
+    },
+    {
+      name: 'Dealership 3',
+      value: '3Deal'
+    },
+    {
+      name: 'Dealership 4',
+      value: '4Deal'
+    },
+    {
+      name: 'Dealership 5',
+      value: '5Deal'
+    },
+    {
+      name: 'Dealership 6',
+      value: '6Deal'
+    },
+    {
+      name: 'Dealership 7',
+      value: '7Deal'
+    },
+    {
+      name: 'Dealership 8',
+      value: '8Deal'
+    },
+    {
+      name: 'Dealership 9',
+      value: '9Deal'
+    },
+    {
+      name: 'Dealership 10',
+      value: '10Deal'
+    }
+  ];
+  stages = [
+    {
+      name: 'All Stages',
+      value: '1Stage'
+    },
+    {
+      name: 'Sales',
+      value: '2Stage'
+    },
+    {
+      name: 'Finance',
+      value: '4Stage'
+    },
+    {
+      name: 'Contract',
+      value: '5Stage'
+    },
+    {
+      name: 'Funding',
+      value: '6Stage'
+    },
+    {
+      name: 'Admin',
+      value: '7Stage'
+    },
+    {
+      name: 'Follow up',
+      value: '8Stage'
+    }
+  ];
+  recents = [
+    {
+      name: 'All activity',
+      value: '1Act'
+    },
+    {
+      name: 'Updated within 24 hours',
+      value: '2Act'
+    },
+    {
+      name: 'Updated within 72 hours',
+      value: '4Act'
+    },
+    {
+      name: 'No activity for 72 hours',
+      value: '5Act'
+    }
+  ];
 
   constructor() { }
 
